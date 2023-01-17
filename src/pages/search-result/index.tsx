1a66cdb98d9caf0e91199323f5c71e25e974cd53
abanoubmesiha <anagy@app-corp.com>
2023-01-17 20:48:34 +0200

    Add filter btn for the web view.

--- src/pages/search-result/index.tsx
+++ src/pages/search-result/index.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { BiFilter } from 'react-icons/bi';
 import searchResultStore from '../../stores/searchResultStore';
 import getSearchResult from '../../actions/searchResultActions';
 import { SearchResultItem } from '../../types/search';
-import './index.css';
 import VideoCard from '../../reusable/video-card';
+import './index.css';
 
 function SearchResult() {
   const [searchResult, setSearchResult] = useState(searchResultStore.getSearchResult());
@@ -29,6 +30,10 @@ function SearchResult() {
             {' '}
             filtered results
           </p>
+          <span className="filter-btn">
+            <BiFilter size={25} />
+            FILTER
+          </span>
         </div>
         <hr />
         {
