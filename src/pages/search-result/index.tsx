2cd8c11088b8f0c58f2635d60c6acb854ae90577
abanoubmesiha <anagy@app-corp.com>
2023-01-17 21:07:51 +0200

    Add web filter options bar.

--- src/pages/search-result/index.tsx
+++ src/pages/search-result/index.tsx
@@ -36,6 +36,25 @@ function SearchResult() {
           </span>
         </div>
         <hr />
+        <div className="filter-options">
+          <span className="option">
+            <p className="type">TYPE</p>
+            <hr />
+            <p className="value">All</p>
+            <p className="value">Channel</p>
+            <p className="value">Playlist</p>
+          </span>
+          <span className="option">
+            <p className="type">UPLOAD DATE</p>
+            <hr />
+            <p className="value">Anytime</p>
+            <p className="value">Today</p>
+            <p className="value">This week</p>
+            <p className="value">This month</p>
+            <p className="value">This year</p>
+          </span>
+        </div>
+        <hr />
         {
           searchResult.items.map(
             (item: SearchResultItem) => <VideoCard key={item.id.videoId} item={item} />,
