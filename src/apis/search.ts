9124aaa342189ccbbab3e2c20453e4b143c3c79e
abanoubmesiha <anagy@app-corp.com>
2023-01-19 14:19:07 +0200

    Use the string istead of the object to pass the params to the search get request

--- src/apis/search.ts
+++ src/apis/search.ts
@@ -1,11 +1,10 @@
 import HttpHelper from './helpers';
-import { SearchParams } from '../types/search';
 
 const key = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 export default {
-  get: (params: SearchParams) => HttpHelper.baseAxios
-    .get(`search?key=${key}&regionCode=us`, { params })
+  get: (paramsUrl: string) => HttpHelper.baseAxios
+    .get(`search?key=${key}&regionCode=us&${paramsUrl}`)
     .then((res) => res.data)
     .catch((err) => err.response.data),
 };
